2f0b250073e493827544a555a9a8b7829fdc27c5
Irvin Tanutan <itanutan@placester.com>
2020-09-13 09:47:28 +0800

    initial commit

---
+++ app/src/main/java/com/irvin/tawkto/activities/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.irvin.tawkto.activities
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuItemCompat.getActionView
+import com.irvin.tawkto.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),
+    androidx.appcompat.widget.SearchView.OnQueryTextListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.search, menu)
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView =
+            getActionView(searchItem) as androidx.appcompat.widget.SearchView
+        searchView.setOnQueryTextListener(this)
+        return true
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        textview.text = newText
+        return false;
+    }
+}
